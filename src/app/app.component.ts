9965aada3da48232c78b9c644f720eaf2d12b770
Aндрей Бабаев <andriyko1979@gmail.com>
2022-09-19 19:32:04 +0300

    my calend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker'
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
+import * as _moment from 'moment';
+const moment = _moment;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DatePipe]
 })
 export class AppComponent {
-  title = 'calend';
+
+  selected: Date | null = null;
+  arrayData: any[] = [];
+  titleEvent: string = "";
+  discriptionEvent: string = "";
+  // startTime: number = "";
+  // endTime: number = "";
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.arrayData, event.previousIndex, event.currentIndex);
+  }
+    
+  constructor(public datepipe: DatePipe) {
+  }
+  saveDate() {
+    if(this.titleEvent===""&&this.discriptionEvent===""&&this.selected===null){
+      return
+    }
+       this.arrayData.push(
+          {value: this.datepipe.transform(this.selected, 'M/d/yy'), 
+    titleEvent:this.titleEvent,
+    discriptionEvent:this.discriptionEvent
+    }
+    )
+    console.log(this.arrayData);
+  }
+  changeCalend() {
+
+    console.log(this.datepipe.transform(this.selected, 'M/d/yy'))
+  }
+  deleteCard(i:number){
+    this.arrayData.splice(i,1)
+        console.log(i);
+    
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker'
+import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD MMMM YYYY',
+  },
+  display: {
+    dateInput: 'DD MMMM YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +31,32 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule,
+    NgxMaterialTimepickerModule,
+    DragDropModule
+   
   ],
-  providers: [],
+  exports: [
+    MatDatepickerModule,
+    NgxMatNativeDateModule,
+    MatMomentDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    MatDatepickerModule,
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
